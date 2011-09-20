ef92363fc46661f82a63209ad30b285bd3f36814
myoon <mike.yoon@scorebig.com>
2011-09-20 15:05:39 -0700

    hacked to support fake it easy automocking

--- Source/StructureMap.AutoFactory/FactoryInterceptor.cs
+++ Source/StructureMap.AutoFactory/FactoryInterceptor.cs
@@ -1,5 +1,5 @@
 using System;
-using Castle.Core.Interceptor;
+using Castle.DynamicProxy;
 
 namespace StructureMap.AutoFactory
 {
---
+++ Source/StructureMap.AutoMocking/FakeItEasyAutoMocker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StructureMap.AutoMocking
+{
+    public class FakeItEasyAutoMocker<TARGETCLASS> : AutoMocker<TARGETCLASS> where TARGETCLASS : class
+    {
+        public FakeItEasyAutoMocker()
+        {
+            _serviceLocator = new FakeItEasyServiceLocator();
+            _container = new AutoMockedContainer(_serviceLocator);
+        }
+    }
+}
